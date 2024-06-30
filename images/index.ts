a752c74f9dfaa03a4043c1cc53693f824fe397e3
b_e_sa <joaospereira963@gmail.com>
2024-06-30 13:18:38 -0300

    feat: images folder

---
+++ images/index.ts
@@ -0,0 +1,9 @@
+import dali from "./dali.jpg"
+import monet from "./monet.jpg";
+import munch from "./munch.jpg";
+import paulRubens from "./paul-rubens.jpg";
+import rembrandt from "./rembrandt.jpg";
+import vanGogh from "./van-gogh.jpg"
+import picasso from "./picasso.jpg"
+
+export { dali, monet, munch, paulRubens, rembrandt, vanGogh, picasso };
